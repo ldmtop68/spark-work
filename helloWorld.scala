012b2b36404d6c6589bcff3f32863d207e2c3aa2
lee dm <ldmtop68@google.com>
2015-07-28 21:16:29 +0900

    수정1

--- helloWorld.scala
+++ helloWorld.scala
@@ -1,9 +1,9 @@
 #!/bin/sh
-  exec scala "$0" "$@"
-    !#
-      object HelloWorld {
-              def main(args: Array[String]) {
-                        println("Hello, world! " + args.toList)
-                                }
-                }
+exec scala "$0" "$@"
+!#
+object HelloWorld {
+        def main(args: Array[String]) {
+        println("Hello, world! " + args.toList)
+    }
+}
 HelloWorld.main(args)
